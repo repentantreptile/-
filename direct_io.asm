d35772a354a2bc0e94dd602cbd92afc0e249a990
repentantreptile <kstknarsenij@gmail.com>
2026-09-22 20:52:20 +0300

    Create direct_io.asm

---
+++ direct_io.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello, Linux!", 0xA
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
